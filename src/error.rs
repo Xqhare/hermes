00ea9e908aa00af9d8936f01148e84ea83d67142
Xqhare <xqhare@web.de>
2026-01-01 10:32:18 +0100

    add(error): getter for wrapped errors

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use nabu::XffValue;
 
 pub type Result<T> = std::result::Result<T, HermesError>;
 
@@ -5,6 +6,36 @@ pub type Result<T> = std::result::Result<T, HermesError>;
 pub enum HermesError {
     PathError(PathError),
     IOError(std::io::Error),
+    NabuError(String),
+    TimeOutError,
+    ServerError(XffValue),
+}
+
+impl HermesError {
+    pub fn get_inner_io_error(&self) -> Option<&std::io::Error> {
+        match self {
+            HermesError::IOError(e) => Some(e),
+            _ => None,
+        }
+    }
+    pub fn get_inner_nabu_error(&self) -> Option<String> {
+        match self {
+            HermesError::NabuError(e) => Some(e.clone()),
+            _ => None,
+        }
+    }
+    pub fn get_inner_server_error(&self) -> Option<XffValue> {
+        match self {
+            HermesError::ServerError(e) => Some(e.clone()),
+            _ => None,
+        }
+    }
+    pub fn get_inner_path_error(&self) -> Option<&PathError> {
+        match self {
+            HermesError::PathError(e) => Some(e),
+            _ => None,
+        }
+    }
 }
 
 #[derive(Debug)]
