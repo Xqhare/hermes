1dcd75f018d8753347f592ddee72a6a2f1c5b450
Xqhare <xqhare@web.de>
2026-01-01 10:33:37 +0100

    add(tests): basic tests

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,26 +1,72 @@
 #[cfg(test)]
 mod tests {
+    use hermes::Hermes;
+    use nabu::XffValue;
     #[test]
     fn new_hermes_valid_path() {
-        use hermes::Hermes;
         let hermes = Hermes::new("tmp/hermes");
         assert!(hermes.is_ok());
-        assert!(std::fs::remove_dir_all("tmp/hermes").is_ok());
+        assert!(std::fs::remove_dir_all("tmp").is_ok());
     }
 
     #[test]
     fn new_hermes_empty_path() {
-        use hermes::Hermes;
         let hermes = Hermes::new("");
         assert!(hermes.is_err());
     }
 
     #[test]
     fn new_hermes_not_directory() {
-        use hermes::Hermes;
         assert!(std::fs::File::create("tmp.data").is_ok());
         let hermes = Hermes::new("tmp.data");
         assert!(hermes.is_err());
         assert!(std::fs::remove_file("tmp.data").is_ok());
+    }
+
+    #[test]
+    fn general_request() {
+        let t = "tmp2";
+        let hermes = Hermes::new(t).unwrap();
+        let value: XffValue = "".into();
+        assert!(hermes.request(value).is_ok());
+        let answer = hermes.await_request();
+        assert!(answer.is_ok());
+        let answer = answer.unwrap();
+        assert_eq!(answer, "".into());
+        assert!(std::fs::remove_dir_all(t).is_ok());
+    }
+
+    #[test]
+    fn req_res_simple() {
+        let t = "tmp3";
+        let hermes = Hermes::new(t).unwrap();
+        let value: XffValue = "Hello".into();
+        assert!(hermes.request(value).is_ok());
+        let req = hermes.await_request();
+        assert!(req.is_ok());
+        let req = req.unwrap();
+        let answer = format!("{} World!", req);
+        assert!(hermes.respond(answer.into()).is_ok());
+        let answer = hermes.await_response();
+        assert!(answer.is_ok());
+        let answer = answer.unwrap();
+        assert_eq!(answer, "Hello World!".into());
+        assert!(std::fs::remove_dir_all(t).is_ok());
+    }
+
+    #[test]
+    fn basic_error_propagation() {
+        let t = "tmp4";
+        let hermes = Hermes::new(t).unwrap();
+        let value: XffValue = "".into();
+        assert!(hermes.request(value).is_ok());
+        assert!(hermes.put_error("error".into()).is_ok());
+        let answer = hermes.await_request();
+        println!("{:?}", answer);
+        let err = answer.unwrap_err();
+        let inner_err = err.get_inner_server_error();
+        assert!(inner_err.is_some());
+        assert_eq!(inner_err.unwrap(), XffValue::from("error"));
+        assert!(std::fs::remove_dir_all(t).is_ok());
     }
 }
